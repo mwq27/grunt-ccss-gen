19fd9086c617d974412836f9444ead68d7c53313
Marques Woodson <marques.woodson@gmail.com>
2013-12-08 14:32:40 -0600

    Starting the new task here.  Getting the local npm not found error for grunt-lib-phantomjs

---
+++ tasks/ccss.js
@@ -0,0 +1,55 @@
+/*
+ * ccss
+ * 
+ *
+ * Copyright (c) 2013 Marques Woodson
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+	// Nodejs libs.
+	var path = require('path');
+//	var phantomjs = require('../node_modules/grunt-contrib-clean').init(grunt);
+
+	grunt.registerMultiTask('ccss', 'Get the Critical CSS of the specified page', function () {
+
+	var errorCount;
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function (file) {
+      // Concat specified files.
+      var src = file.src.filter(function (filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function (filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.separator));
+	    console.log(file);
+      // Handle options.
+      src += options.punctuation;
+
+      // Write the destination file.
+      grunt.file.write(file.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + file.dest + '" created.');
+    });
+  });
+
+};
